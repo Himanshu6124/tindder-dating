f433ecb7b0cd74b67a7941e13e3b97de1e8e1a26
Himanshu Pathsaria <himanshu6124@gmail.com>
2024-04-10 19:49:57 +0530

    first commit

---
+++ app/src/main/java/com/tinder/compose/MainActivity.kt
@@ -0,0 +1,332 @@
+package com.tinder.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.himanshu.compose.R
+import kotlinx.coroutines.delay
+import kotlin.math.roundToInt
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+
+            val navController: NavHostController = rememberNavController()
+//                val bottomBarHeight = 56.dp
+//                val bottomBarOffsetHeightPx = remember { mutableStateOf(0f) }
+            val buttonsVisible = remember { mutableStateOf(true) }
+
+            Scaffold(
+                bottomBar = {
+                    BottomBar(navController, buttonsVisible, Modifier)
+                }) { paddingValues ->
+                Box(
+                    modifier = Modifier.padding(paddingValues)
+                ) {
+                    NavigationGraph(navController = navController)
+                }
+            }
+
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card(modifier: Modifier) {
+
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier
+            .background(Color.Black)
+            .background(Color.LightGray)
+            .paint(painterResource(id = R.drawable.card_image), contentScale = ContentScale.Crop)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth(.9f)
+                .align(Alignment.Bottom)
+                .padding(bottom = 30.dp)
+        ) {
+            Image(painter = painterResource(id = R.drawable.restart), contentDescription = "rewind")
+            Image(painter = painterResource(id = R.drawable.cancel), contentDescription = "cancel")
+            Image(
+                painter = painterResource(id = R.drawable.favourites),
+                contentDescription = "star"
+            )
+            Image(painter = painterResource(id = R.drawable.heart), contentDescription = "heart")
+            Image(painter = painterResource(id = R.drawable.flash), contentDescription = "flash")
+
+        }
+    }
+
+}
+
+@Composable
+fun SwipeCard(
+    onSwipeLeft: () -> Unit = {},
+    onSwipeRight: () -> Unit = {},
+    swipeThreshold: Float = 400f,
+    sensitivityFactor: Float = 3f,
+    content: @Composable () -> Unit
+) {
+    // Implementation goes here
+
+    var offset by remember { mutableFloatStateOf(0f) }
+    var dismissRight by remember { mutableStateOf(false) }
+    var dismissLeft by remember { mutableStateOf(false) }
+    val density = LocalDensity.current.density
+
+    LaunchedEffect(dismissRight) {
+        if (dismissRight) {
+            delay(300)
+            onSwipeRight.invoke()
+            dismissRight = false
+        }
+    }
+
+    LaunchedEffect(dismissLeft) {
+        if (dismissLeft) {
+            delay(300)
+            onSwipeLeft.invoke()
+            dismissLeft = false
+        }
+    }
+
+    Box(modifier = Modifier
+        .offset { IntOffset(offset.roundToInt(), 0) }
+        .pointerInput(Unit) {
+            detectHorizontalDragGestures(onDragEnd = {
+                offset = 0f
+            }) { change, dragAmount ->
+
+                offset += (dragAmount / density) * sensitivityFactor
+                when {
+                    offset > swipeThreshold -> {
+                        dismissRight = true
+                    }
+
+                    offset < -swipeThreshold -> {
+                        dismissLeft = true
+                    }
+                }
+                if (change.positionChange() != Offset.Zero) change.consume()
+            }
+        }
+        .graphicsLayer(
+            alpha = 10f - animateFloatAsState(if (dismissRight) 1f else 0f, label = "").value,
+            rotationZ = animateFloatAsState(offset / 50, label = "").value
+        )) {
+        content()
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 80.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        SwipeCard {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(.9f)
+                    .fillMaxHeight(.8f)
+                    .clip(RoundedCornerShape(60.dp))
+                    .background(Color.LightGray),
+            )
+        }
+
+    }
+
+}
+
+@Composable
+fun FavouriteScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Magenta)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Favourite Screen",
+            style = MaterialTheme.typography.titleLarge,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Composable
+fun NotificationScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Blue)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Notification Screen",
+            style = MaterialTheme.typography.titleLarge,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+
+sealed class Destinations(
+    val route: String,
+    val title: String? = null,
+    val icon: ImageVector? = null
+) {
+    data object HomeScreen : Destinations(
+        route = "home_screen",
+        title = "Home",
+        icon = Icons.Outlined.Home
+    )
+
+    data object Favourite : Destinations(
+        route = "favourite_screen",
+        title = "Favorite",
+        icon = Icons.Outlined.Favorite
+    )
+
+    data object Notification : Destinations(
+        route = "notification_screen",
+        title = "Notification",
+        icon = Icons.Outlined.Notifications
+    )
+
+}
+
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController, startDestination = Destinations.HomeScreen.route) {
+        composable(Destinations.HomeScreen.route) {
+            HomeScreen()
+        }
+        composable(Destinations.Favourite.route) {
+            FavouriteScreen()
+        }
+        composable(Destinations.Notification.route) {
+            NotificationScreen()
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    navController: NavHostController, state: MutableState<Boolean>, modifier: Modifier = Modifier
+) {
+    val screens = listOf(
+        Destinations.HomeScreen, Destinations.Favourite, Destinations.Notification
+    )
+
+    NavigationBar(
+        modifier = modifier,
+        containerColor = Color.LightGray,
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        screens.forEach { screen ->
+
+            NavigationBarItem(
+                label = {
+                    Text(text = screen.title!!)
+                },
+                icon = {
+                    Icon(imageVector = screen.icon!!, contentDescription = "")
+                },
+                selected = currentRoute == screen.route,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    unselectedTextColor = Color.Gray, selectedTextColor = Color.White
+                ),
+            )
+        }
+    }
+
+}
---
+++ app/src/main/java/com/tinder/compose/Screen.kt
@@ -0,0 +1,61 @@
+package com.tinder.compose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.himanshu.compose.R
+
+
+@Preview
+    @Composable
+    fun BottomBar() {
+        BottomAppBar(
+            contentColor = Color.White,
+            containerColor = Color.Black,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .padding(5.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Image(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(50.dp),
+                    painter = painterResource(id = R.drawable.home),
+                    contentDescription = "Home"
+                )
+                Image(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(50.dp),
+                    painter = painterResource(id = R.drawable.search),
+                    contentDescription = "Search"
+                )
+                Image(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(50.dp),
+                    painter = painterResource(id = R.drawable.book),
+                    contentDescription = "Library"
+                )
+            }
+        }
+    }
+
